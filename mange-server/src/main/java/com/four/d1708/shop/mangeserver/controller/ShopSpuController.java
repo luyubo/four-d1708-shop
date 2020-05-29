d255355fae015de440b7b34416eb214632fa8dc1
luyubo <6553225+luyubo@user.noreply.gitee.com>
2020-05-29 23:28:48 +0800

    商品spu的跨域处理

--- mange-server/src/main/java/com/four/d1708/shop/mangeserver/controller/ShopSpuController.java
+++ mange-server/src/main/java/com/four/d1708/shop/mangeserver/controller/ShopSpuController.java
@@ -22,11 +22,20 @@ public class ShopSpuController {
     @Autowired
     private ISpuService iSpuService;
 
+    /**
+     * @Author luyubo
+     * @Description 商品spu分页查询
+     * @Date  2020/5/29 23:26
+     * @Param [pageNum:当前页, pageSize:每页分页几条, shopSpuVo:条件查询]
+     * @return java.lang.Object
+     **/
     @RequestMapping("/findAll")
     public Object findAll(@RequestParam(value = "pageNum",defaultValue = "1")Integer pageNum,
                           @RequestParam(value = "pageSize",defaultValue = "3")Integer pageSize,
                           ShopSpuVo shopSpuVo){
+        //分页
         Page page=new Page(pageNum,pageSize);
+        //查询
         return iSpuService.findAll(page,shopSpuVo);
     }
 }
